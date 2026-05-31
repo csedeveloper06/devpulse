3699d8cd319ee8c7fbfacc2ea5a045bd261920f6
Farida <you@example.com>
2026-05-31 22:48:56 +0600

    added issues table

--- src/db/index.ts
+++ src/db/index.ts
@@ -28,6 +28,52 @@ export const initDB = async () => {
           updated_at TIMESTAMP DEFAULT NOW()
         )
       `);
+
+    await pool.query(`
+
+      DO $$
+      BEGIN
+        IF NOT EXISTS (
+          SELECT 1
+          FROM pg_type
+          WHERE typname = 'issue_type'
+          ) THEN
+          CREATE TYPE issue_type AS ENUM (
+          'bug',
+          'feature_request'
+          );
+        END IF;
+      END $$;
+      
+
+      DO $$
+      BEGIN
+        IF NOT EXISTS (
+          SELECT 1
+          FROM pg_type
+          WHERE typname = 'issue_status'
+          ) THEN
+          CREATE TYPE issue_status AS ENUM (
+          'open',
+          'in_progress',
+          'resolved'
+          );
+        END IF;
+      END $$;
+
+       CREATE TABLE IF NOT EXISTS issues(
+         id SERIAL PRIMARY KEY,
+         title VARCHAR(150) NOT NULL,
+         description TEXT NOT NULL,
+         type issue_type NOT NULL,
+         status issue_status NOT NULL DEFAULT 'open',
+         reporter_id INT UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+         
+         created_at TIMESTAMP DEFAULT NOW(),
+         updated_at TIMESTAMP DEFAULT NOW()
+       )
+    
+      `);
     console.log("database connected successfully!");
   } catch (error) {
     console.log(error);
